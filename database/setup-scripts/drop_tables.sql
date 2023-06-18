82ce308c11664dde8acf122f73a617eaec1d4ce7
Grzegorz <grzegorz.siudak.stud@pw.edu.pl>
2023-06-18 20:24:15 +0200

    deleted drop_tables

--- database/setup-scripts/drop_tables.sql
+++
@@ -1,7 +0,0 @@
---/
-BEGIN
-  FOR cur_tab IN (SELECT table_name FROM all_tables WHERE owner = 'C##DEVELOP') LOOP
-    EXECUTE IMMEDIATE 'DROP TABLE ' || cur_tab.table_name || ' CASCADE CONSTRAINTS';
-  END LOOP;
-END;
-/
